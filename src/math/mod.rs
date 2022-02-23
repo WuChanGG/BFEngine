661a150c6c85fe179ab6ffb8da59776327161ee9
WuChanGG <krulex.em@gmail.com>
2022-02-23 16:00:50 -0600

    progress on the core Vector3D functionality

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -46,3 +46,89 @@ impl ops::Index<usize> for Vector3D {
         }
     }
 }
+
+impl ops::IndexMut<usize> for Vector3D {
+    fn index_mut<'a>(&'a mut self, i: usize)
+        -> &'a mut f32 {
+        match i {
+            0 => return &mut self.x,
+            1 => return &mut self.y,
+            2 => return &mut self.z,
+            _ => panic!("Vector::Index index out of bounds")
+        }
+    }
+}
+
+impl ops::MulAssign<f32> for Vector3D {
+    fn mul_assign(&mut self, rhs: f32) {
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+    }
+}
+
+impl ops::DivAssign<f32> for Vector3D {
+    fn div_assign(&mut self, mut rhs: f32) {
+        rhs = 1.0 / rhs;
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+    }
+}
+
+impl ops::Mul<f32> for Vector3D {
+    type Output = Vector3D;
+    fn mul(self, rhs: f32) -> Self::Output {
+        return Vector3D {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z *rhs,
+        };
+    }
+}
+
+impl ops::Div<f32> for &Vector3D {
+    type Output = Vector3D;
+    fn div(self, mut rhs: f32) -> Self::Output {
+        rhs = 1.0 / rhs;
+        return Vector3D {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z *rhs,
+        };
+    }
+}
+
+impl ops::Div<f32> for Vector3D {
+    type Output = Vector3D;
+    fn div(self, mut rhs: f32) -> Self::Output {
+        rhs = 1.0 / rhs;
+        return Vector3D {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z *rhs,
+        };
+    }
+}
+
+impl ops::Neg for Vector3D {
+    type Output = Vector3D;
+    fn neg(self) -> Self::Output {
+        return Vector3D {
+            x: -self.x,
+            y: -self.y,
+            z: -self.z
+        };
+    }
+}
+
+impl Vector3D {
+    fn magnitude(&self) -> f32 {
+        return f32::sqrt(
+            self.x * self.x + self.y * self.y + self.z * self.z)
+    }
+
+    fn normalize(&self) -> Vector3D {
+        return self / self.magnitude();
+    }
+}
